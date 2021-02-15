ccebc29dd5840a0c3ee5ba983114aca9140d5a83
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-02-15 10:09:54 +0530

    Create 56 - Delegates.kt

---
+++ 56 - Delegates.kt
@@ -0,0 +1,54 @@
+/**
+ * Delegates mean to remove boilerplate code (repeat code)
+ * Delegates happen when multiple inheritance
+ * Ex. Following code
+ *
+ * 2 interface Downloader and Player which extends to FileDownloader, and FilePlayer
+ * both class (FileDownloader, and FilePlayer) overrides interface method
+ *
+ * create 3rd class which implement both interfaces it create error to override its method
+ *
+ * here we use delegates at XMedia
+ *
+ *
+ * interface Downloader             interface Player
+ *          |                               |
+ * class FileDownloader             class FilePlayer
+ *              |                       |
+ *           class XMedia (FileDownloader, FilePlayer) : Downloader by fileDownloader, Player by filePlayer  <------- use delegates
+ *
+ *
+ *
+ *Ref. simplifiedcoding
+ * */
+
+interface Downloader {
+    fun download()
+}
+
+interface Player {
+    fun play()
+}
+
+class FileDownloader() : Downloader{
+    override fun download() {
+        println("file downloading")
+    }
+}
+
+class FilePlayer : Player{
+    override fun play() {
+        println("file playing")
+    }
+}
+
+class XMedia(val fileDownloader: FileDownloader, val filePlayer: FilePlayer) : Downloader by fileDownloader,Player by filePlayer
+
+fun main() {
+    val media = XMedia(FileDownloader(),FilePlayer())
+    media.download()
+    media.play()
+    println()
+    media.fileDownloader.download()
+    media.filePlayer.play()
+}
