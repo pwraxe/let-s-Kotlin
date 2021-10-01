007be325ce7def07db0e633601f8e7c91e28487a
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-10-01 11:24:13 +0530

    Update 53 - functions.kt

--- 53 - functions.kt
+++ 53 - functions.kt
@@ -25,3 +25,28 @@ Mobile : 9999999999
 Name : A S P 
 Email : anand.codexdroid@gmail.com 
 Mobile : 8888888888
+
+
+_____________________________________________________________________________________________________
+
+// function inside the function
+
+fun main() {
+    fun show(){
+        println("fun in fun")
+    }
+    println("fun")
+    show()
+}
+----------------------------------------------------------------------------------------------------
+
+class Main {
+
+    fun show(num: Int) {
+        println("HE : $num")
+        fun getNum(): Int = 1432
+        println("SHE : ${getNum()}")
+    }
+}
+
+fun main() = Main().show(143)
