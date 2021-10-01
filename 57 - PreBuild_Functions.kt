c95be9e65476ccdd836d1370b677a5c299711ae9
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-10-01 20:17:01 +0530

    Update 57 - PreBuild_Functions.kt

--- 57 - PreBuild_Functions.kt
+++ 57 - PreBuild_Functions.kt
@@ -10,3 +10,19 @@ fun main() {
     println(nums.zip(nums)) //  [(1, 1), (2, 2), (3, 3), (4, 4)]
     println(text.zip(text)) //  [(One, One), (Two, Two), (Three, Three), (Four, Four)]
 }
+____________________________________________________________________________________________
+
+-------> flatten()
+
+fun main() {
+    val numberSets = listOf(listOf("A","C","B"), listOf("1",2,'C'), listOf(3.14,true,0b111))
+    println(numberSets.flatten()) // [A, C, B, 1, 2, C, 3.14, true, 7]
+}
+
+___________________________________________________________________________________________
+
+-------> joinToString()
+fun main() {
+    val text = listOf("A","B","C","D","E","F","G")
+    println(text.joinToString())    // A, B, C, D, E, F, G
+}
