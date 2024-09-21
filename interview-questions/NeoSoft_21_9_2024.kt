0baa88a28fe259ff70c88a05fd30bee4f7e760bb
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-09-21 18:53:50 +0530

    Create NeoSoft_21_9_2024.kt

---
+++ interview-questions/NeoSoft_21_9_2024.kt
@@ -0,0 +1,25 @@
+-> Introduction
+-> What is abstraction
+-> Different between abstraction and inheritance
+-> Map Vs List
+-> What is Homogeneous heterogeneous
+-> Kotlin Scope functions
+-> difference between let and run 
+-> will run work on nullable variable
+-> How many Android Component you worked on.
+-> What is Coroutine
+-> How many scopes function in coroutine
+-> what is inline
+-> what is extension function (Write Extension function which returns first char of String)
+-> What is Interceptors
+-> Activity Lifecycle
+-> Fragment Lifecycle
+
+
+fun main() {
+	val name = "Akshay"
+	name.getFirst()
+}
+fun String.getFirst(): Char {
+	return this[0]
+}
