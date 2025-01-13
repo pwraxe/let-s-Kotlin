f484553b934f482ad45c555d9d79664860a3c773
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2025-01-13 12:58:21 +0530

    Create GeneralNotes.kt

---
+++ DesignPatterns/GeneralNotes.kt
@@ -0,0 +1,30 @@
+1. Creational Patterns
+	-> Singleton Pattern
+		- No More than 1 object
+
+	- Factory Pattern
+		- Return object of class based on input requirement
+
+	- Abstract Factory
+		- interface returen instance of main interface
+
+	- Builder Pattern
+		- create object based on given constrints
+
+	- Prototype Pattern
+		- copy existing object to create new one
+
+
+2. Structural Patterns
+	-> Adapter Pattern
+		- Converts one interface to another that a client expects.
+
+	-> Bridge
+		- Seperate abstractoin from its implemntion
+
+3. 3. Behavioral Patterns
+	-> Observer
+		Observe changes 
+
+	-> Strategy
+		 Defines different ways to solved problem, we decide which one is suitable
